c30f45f4e778cc7876e73c73f4616f545874fec0
Antony <vontonnie@gmail.com>
2021-01-08 09:48:14 +0300

    Build Retrofit Object

--- app/src/main/java/com/udacity/asteroidradar/api/NeoWService.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NeoWService.kt
@@ -3,6 +3,8 @@ package com.udacity.asteroidradar.api
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class NeoWService {
 
@@ -11,7 +13,13 @@ class NeoWService {
     private val BASE_URL = Constants.BASE_URL
 
     //Build Moshi Object
-    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
 
-    
+    //Build Retrofit Object
+    val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .build()
 }
