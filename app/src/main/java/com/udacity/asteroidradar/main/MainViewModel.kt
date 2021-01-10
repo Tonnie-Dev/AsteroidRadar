f193691981c4881501781f51fead921acdbdd0cb
Antony <vontonnie@gmail.com>
2021-01-10 11:24:25 +0300

    Make Progress bar to be at the centre of RecyclerView

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -2,13 +2,11 @@ package com.udacity.asteroidradar.main
 
 import android.app.Application
 import androidx.lifecycle.*
-import com.udacity.asteroidradar.Asteroid
-import com.udacity.asteroidradar.Constants
-import com.udacity.asteroidradar.PictureOfDay
-import com.udacity.asteroidradar.Utils
+import com.udacity.asteroidradar.*
 import com.udacity.asteroidradar.api.NeoWService
 import com.udacity.asteroidradar.api.parseJSONStringResponse
 import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.repo.AsteroidRepo
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.text.ParseException
@@ -20,12 +18,15 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
 
     //instantiate database
-    val database = AsteroidDatabase.getDatabaseInstance()
+    val database = AsteroidDatabase.getDatabaseInstance(application)
+
+    //create repo
+    val repo = AsteroidRepo(database)
 
 
     private val _asteroidList = MutableLiveData<List<Asteroid>>()
     val asteroidList: LiveData<List<Asteroid>>
-    get() = _asteroidList
+    get() = repo.asteroidLiveData
 
     private val _picture = MutableLiveData<PictureOfDay>()
     val picture:LiveData<PictureOfDay>
@@ -35,7 +36,13 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     val loading:LiveData<Boolean>
         get() = _loading
     init {
-        getAsteroids()
+
+        viewModelScope.launch {
+           // repo.getAsteroidsListFromNetwork(DateFilter.WEEK_ASTEROIDS)
+
+        }
+
+       // getAsteroids()
         getPictureOfTheDay()
 
         Timber.i("Date is ${Utils.getTodayDate()}")
@@ -61,7 +68,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
                 val parsedResponse = parseJSONStringResponse(response)
                 _loading.value = false
-                _asteroidList.value = parsedResponse
+               // _asteroidList.value = repo.asteroidLiveData
               // Timber.i("The raw data returned is $parsedResponse")
             } catch (e: Exception) {
                 Timber.i("The raw data returned is $e")
