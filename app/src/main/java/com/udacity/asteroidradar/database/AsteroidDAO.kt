d7c7b627d1c3c5135e80c5a9a49198283cb21d7d
Antony <vontonnie@gmail.com>
2021-01-10 08:57:36 +0300

    Create Database Class

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDAO.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDAO.kt
@@ -1,11 +1,21 @@
 package com.udacity.asteroidradar.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
-
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
 
 @Dao
-class AsteroidDAO {
+interface AsteroidDAO {
+
+    //Return a LiveData to make owners observe LiveData Automatically
+    @Query("SELECT * FROM asteroidentity")
+    fun getAsteroids(): LiveData<List<AsteroidEntity>>
 
 
+    //Insert Videos - varags
+    @Insert(onConflict = REPLACE)
+    fun insertAsteroids(vararg asteroids: AsteroidEntity)
 
 }
