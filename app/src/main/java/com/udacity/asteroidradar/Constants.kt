5abb439430a0889789524afe0b5dc58b851b5bca
Antony <vontonnie@gmail.com>
2021-01-12 09:01:32 +0300

    Omit the API_KEY in preparation for submission

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -2,7 +2,8 @@ package com.udacity.asteroidradar
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
-    const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "cQFHop6TptuKuyX1784hIC86YWgsWuFqOsxUTYoI"
+
+    //API KEY IS OMITTED
+    const val API_KEY = "OMITTED!!!!!!"
 }
